8bc8850aa540b013faa03ff1bb979f253a339ea1
jongwon.Yoo <ryu01410@gmail.com>
2021-09-30 23:12:58 +0900

    2021.09.14

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,13 +1,16 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  //protected를 생성자를 만들어줌  Protected로 제약하는 경우 new 생성 방지 때문
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -24,5 +27,32 @@ public class OrderItem {
 
     private int orderPrice;     //주문 당시 가격
     private int count;          //주문 당시 수량
+
+//    protected OrderItem(){
+//
+//    }
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        //JPA의 장점
+        //도메인의 값을 수정할 경우 JPA가 더티체킹을 통해서 알아서 값을 수정하는 UPDATE 쿼리를 날려줌
+        //더티체킹 : 변경내역 감지
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 
 }
