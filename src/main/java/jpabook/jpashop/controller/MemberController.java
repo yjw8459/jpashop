cf9d81fcf7acdd074a41ec4cba08eda321c846c7
jongwon.Yoo <ryu01410@gmail.com>
2021-10-11 01:48:35 +0900

    2021.09.14

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -1,10 +1,16 @@
 package jpabook.jpashop.controller;
 
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,5 +22,23 @@ public class MemberController {
     public String createForm(Model model){  //model :
         model.addAttribute("memberForm", new MemberForm()); //빈 껍데기를 화면에 넘김
         return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result){
+        //## 꿀팁
+        //BindingResult를 사용하면 오류 페이지로 가지 않고 오류 내용이 result에 담기고 코드가 실행됌.
+        if (result.hasErrors()){
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getCity());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        membverService.join(member);
+        return "redirect:/";
     }
 }
--- src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -3,10 +3,15 @@ package jpabook.jpashop.controller;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+
+//Member 도메인 엔티티로도 사용할 수 있지만 도메인에서 원하는 벨리데이션과 컨트롤러에서 원하는 벨리데이션이 다르다.
+//컨트롤러에서 원하는 벨리데이션에 핏된 DTO를 따로 사용하는 것이 가독성 면에서 좋고 편리할 수 있다.
 @Getter @Setter
 public class MemberForm {
 
-    //@NotEmpty(message="회원 이름은 필수입니다.")
+    //에러 시 name에 메세지가 주입됌
+    @NotEmpty(message="회원 이름은 필수입니다.")
     private String name;
 
     private String city;
