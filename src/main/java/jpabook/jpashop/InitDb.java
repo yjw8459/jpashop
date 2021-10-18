1be94ab0bed0919beb7b90237f68e713f67ffab1
jongwon.Yoo <ryu01410@gmail.com>
2021-10-19 00:22:21 +0900

    2021.10.14

--- src/main/java/jpabook/jpashop/InitDb.java
+++ src/main/java/jpabook/jpashop/InitDb.java
@@ -8,8 +8,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 총 주문 2개
@@ -28,6 +26,11 @@ public class InitDb {
 
     private final InitService initService;
 
+    /**
+     * 서버 띄우면 컴포넌트 스캔을 읽으면서
+     * 스프링 빈이 다 엮이고 마지막에 PostConstruct 스프링이 호출
+     *
+     */
     @PostConstruct
     public void init(){ //애플리케이션 실행 시점에 실행하기 위함.
         initService.dbInit1();
@@ -40,32 +43,64 @@ public class InitDb {
 
         private final EntityManager em;
         public void dbInit1(){
-            Member member = new Member();
-            member.setName("userA");
-            member.setAddress(new Address("서울", "1", "11111"));
+            Member member = createMember("userA", "서울", "1", "1111");
             em.persist(member); //member를 영속상태로 만들어줌
 
 
-            Book book1 = new Book();
-            book1.setName("JPA1 BOOK");
-            book1.setPrice(10000);
-            book1.setStockQuantity(100); //재고 수량
+            Book book1 = createBook("JPA1 BOOK", 10000, 100);
             em.persist(book1);
 
-            Book book2 = new Book();
-            book2.setName("JPA2 BOOK");
-            book2.setPrice(20000);
-            book2.setStockQuantity(100); //재고 수량
+            Book book2 = createBook("JPA2 BOOK", 20000, 100);
             em.persist(book2);
 
             OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);//book1을 10000에 구매
             OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 1);//book2을 20000에 구매
 
-            Delivery delivery = new Delivery();
-            delivery.setAddress(member.getAddress());   //고객의 주소
+            Delivery delivery = createDelivery(member);
             Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);//...문법으로 orderItem1, orderItem2
             //...문법은 List는 사용X
             em.persist(order);      //order 영속화
+        }
+
+        private Delivery createDelivery(Member member) {
+            Delivery delivery = new Delivery();
+            delivery.setAddress(member.getAddress());   //고객의 주소
+            return delivery;
+        }
+
+        private Book createBook(String name, int price, int stockQuantity) {
+            Book book1 = new Book();
+            book1.setName(name);
+            book1.setPrice(price);
+            book1.setStockQuantity(stockQuantity); //재고 수량
+            return book1;
+        }
+
+        public void dbInit2(){
+            Member member = createMember("userB", "진주", "2", "2222");
+            em.persist(member); //member를 영속상태로 만들어줌
+
+
+            Book book1 = createBook("SPRING1 BOOK", 20000, 200);
+            em.persist(book1);
+
+            Book book2 = createBook("SPRING2 BOOK", 40000, 300);
+            em.persist(book2);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);//book1을 10000에 구매
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 1);//book2을 20000에 구매
+
+            Delivery delivery = createDelivery(member);
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);//...문법으로 orderItem1, orderItem2
+            //...문법은 List는 사용X
+            em.persist(order);      //order 영속화
+        }
+
+        private Member createMember(String name, String city, String street, String zipcode) {
+            Member member = new Member();
+            member.setName(name);
+            member.setAddress(new Address(city, street, zipcode));
+            return member;
         }
 
     }
